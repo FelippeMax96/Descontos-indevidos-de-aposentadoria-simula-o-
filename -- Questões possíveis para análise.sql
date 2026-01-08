1fbabc1c4dfe36817b41ff1655ba1ca3692c390f
Felippe Max <felippe1902@gmail.com>
2026-01-08 13:07:55 -0300

    Add potential questions for analysis in SQL file
    Atualizei a tabela.

--- -- Questões possíveis para análise.sql
+++ -- Questões possíveis para análise.sql
@@ -1,3 +1,23 @@
+1	23	449.96	2025-07-03	Atraso na revisão
+2	22	321.65	2024-11-11	Atraso na revisão
+3	39	453.7	2025-04-29	Cobrança indevida
+4	32	438.63	2025-05-10	Cobrança indevida
+5	14	406.91	2024-11-10	Atraso na revisão
+6	48	112.06	2024-10-10	Erro administrativo
+7	47	168.94	2024-11-19	Atraso na revisão
+8	41	163.05	2024-11-02	Duplicidade de benefício
+9	10	247.17	2025-08-26	Cobrança indevida
+10	35	430.16	2024-11-27	Cobrança indevida
+11	11	107.07	2024-10-22	Cobrança indevida
+12	42	473.18	2024-11-07	Atraso na revisão
+13	19	182.33	2025-01-13	Erro administrativo
+14	46	434.8	2024-10-29	Atraso na revisão
+15	7	140.61	2024-10-09	Duplicidade de benefício
+
+
+
+
+
 -- Questões possíveis para análise
 
 -- 1.Quantos descontos indevidos foram registrados no total?
